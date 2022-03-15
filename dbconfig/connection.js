222858fa427478023542f5eda4b9ddd4aa642164
Alalinga <alalinga.mubarik@amalitech.org>
2022-03-15 17:53:17 +0000

    added mongoose database connection

---
+++ dbconfig/connection.js
@@ -0,0 +1,7 @@
+
+exports.module = function dbConnection(db,url){
+  return db.connect(url,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then((re)=>{
+        console.log(" mongoose connected successfully!");
+     }).catch(error=>console.log(" Error occured => "+error))
+}
