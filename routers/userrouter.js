6ed6478d8edb449849c23bbbe650f4e793148ad8
Alalinga <alalinga.mubarik@amalitech.org>
2022-03-16 05:48:11 +0000

    finished single image upload

--- routers/userrouter.js
+++ routers/userrouter.js
@@ -36,7 +36,7 @@ route.post('/create-accounts', async (req, res) => {
 
 
 //  Videos routes
-route.post('/upload-videos',upload.single("video"), (req, res, next) => {
+route.post('/upload-videos', upload.single("video"), (req, res, next) => {
     try {
         // const response = await cloudinary.upload.upload(req.file.path)
 
@@ -53,19 +53,57 @@ route.get('/vedios', async (req, res, next) => {
 
 
 // Images routes
-route.post('/upload-images',checkAuthentication,uploader.array('image'),  (req, res) => {
-    
-       if(req.files){
+route.post('/upload-images', checkAuthentication, uploader.array('image'), (req, res) => {
+
+    if (req.files) {
         // const response = await cloudinary.uploader.upload(req.file.path,{
         //     folder:`techBuddy/${req.user}/`
         // })
-        res.json("ok I resive somth => "+ req.files)
-       
-       }else{
-           res.json("something went wrong"+ req.files)
-       }
+
+        res.json("ok I resive somth => " + req.files + " auth " + req.user._id)
+
+    } else {
+        res.json("something went wrong" + req.files)
+    }
 });
 
+// upload Single Image
+route.post('/upload-single-image', checkAuthentication, upload.single('image'), async (req, res) => {
+    try {
+        if (req.file) {
+            console.log("I am in")
+            // , { folder: `techBuddy/${req.user.username}/` }
+            const response = await cloudinary.uploader.upload(req.file.path, { folder: `techBuddy/${req.user.username}/images` });
+
+            const user = await User.findById(req.user._id)
+            if (user) {
+                user.images.push({ name: response.original_filename, url: response.secure_url,cloundinary_key:response.public_id })
+                user.save((error,results)=>{
+                    if(error)return console.log(error)
+                    console.log("data saved")
+                    return res.json(results)
+                })
+                // res.json(response)
+            }
+
+        }
+    } catch (e) {
+        new Error(e)
+    }
+})
+
+
+// get single inage
+route.get('/images/:id', (req, res, next) => {
+    try {
+        // const response = await cloudinary.upload.upload(req.file.path)
+        res.json(req.file)
+
+    } catch (e) {
+        res.json(e)
+    }
+})
+//  get all Images
 route.get('/images', (req, res, next) => {
     try {
         // const response = await cloudinary.upload.upload(req.file.path)
