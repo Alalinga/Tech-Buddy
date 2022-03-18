9dc774dd845d5467ee2257a50835679a8f44564e
Alalinga <alalinga.mubarik@amalitech.org>
2022-03-18 23:38:18 +0000

    added fetch images and videos functionalities

--- models/operations.js
+++ models/operations.js
@@ -56,7 +56,7 @@ const deleteImage = async (path, userId) => {
                     await user.save()
                     return { success: "Successfully deleted image with name " + path }
                 } catch (e) {
-                    return { error: e }
+                    return { error:"error occured "+ e }
                 }
             }
             return response
@@ -74,24 +74,77 @@ const deleteVideo = async (path, userId) => {
     if (user) {
         try {
             const video = user.videos.filter(ele => ele.name == path)
-               console.log(video[0].cloundinary_key)
+            
             if (!video.length) return { error: "Video with name " + path + " not found" }
-            const response = await cloudinary.uploader.destroy(video[0].cloundinary_key,{resource_type: 'video'})
+            const response = await cloudinary.uploader.destroy(video[0].cloundinary_key, { resource_type: 'video' })
             if (response.result === 'ok') {
                 try {
                     user.videos.pull(video[0]._id)
                     await user.save()
                     return { success: "Successfully deleted video with name " + path }
                 } catch (e) {
-                    return { error: e }
+                    return { error: "error database "+ e }
                 }
             }
             return response
         } catch (e) {
-            return { error: e }
+            return { error:"error occured "+ e }
         }
     }
     return { error: "User not found" }
+}
+
+const getAllImages = async (userId) => {
+    try {
+       const user  =await User.findById(userId)
+       return user.images
+
+    } catch (e) {
+        return { error: "error occured "+ e }
+    }
+
+}
+const getSingleImage = async (userId,imageId) => {
+    try {
+       const user  =await User.findById(userId)
+       return user.images.filter(ele=>ele.name===imageId)
+
+    } catch (e) {
+        return { error:"error occured "+ e }
+    }
+
+}
+
+const getAllVideos = async (userId) => {
+    try {
+       const user  = await User.findById(userId)
+       return user.videos
+
+    } catch (e) {
+        return { error:"error occured "+ e }
+    }
+
+}
+const getSingleVideo = async (userId,filename) => {
+    try {
+       const user  =await User.findById(userId)
+       return user.videos.filter(ele=>ele.name===filename)
+
+    } catch (e) {
+        return { error:"error occured "+ e }
+    }
+
+}
+
+
+
+const updateFile = async (path, user) => {
+    try {
+        await cloudinary.api.update()
+    } catch (e) {
+        return { error:"error occured "+ e }
+    }
+
 }
 
 
@@ -100,5 +153,9 @@ module.exports =
     uploadFile,
     uploadBigFile,
     deleteImage,
-    deleteVideo
+    deleteVideo,
+    getAllImages,
+    getAllVideos,
+    getSingleImage,
+    getSingleVideo
 }
