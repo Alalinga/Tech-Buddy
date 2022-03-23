817a7b429f1c2abdc7c605ece1cbec3730ce5888
Alalinga <alalinga.mubarik@amalitech.org>
2022-03-23 03:37:07 +0000

    removed dbconfig folder

--- dbconfig/connection.js
+++
@@ -1,7 +0,0 @@
-
-exports.module = function dbConnection(db,url){
-  return db.connect(url,{useNewUrlParser:true,useUnifiedTopology:true})
-    .then((re)=>{
-        console.log(" mongoose connected successfully!");
-     }).catch(error=>console.log(" Error occured => "+error))
-}
