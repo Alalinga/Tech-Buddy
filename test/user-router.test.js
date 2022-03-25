ee53ac32b71de0073c2093f03cbd5155e82758fc
Alalinga <alalinga.mubarik@amalitech.org>
2022-03-25 03:36:03 +0000

    modified code

--- test/user-router.test.js
+++ test/user-router.test.js
@@ -8,6 +8,8 @@ const fs = require('fs')
 const expect = chai.expect
 chai.use(http)
 const cookie = '';
+let imageName = ''
+let videoName = ''
 
 
 
@@ -20,8 +22,6 @@ describe("User routes", function () {
             .catch((e) => done(e))
 
     });
-
-
 
 
 
@@ -103,26 +103,85 @@ describe("User routes", function () {
 
     /** test cases for Images */
 
-    describe("GET, Get Uploaded Images", function () {
+    describe("POST, User upload Images", function () {
+        it("User should be able to upload single or multiple images", function (done) {
+            this.timeout(20000)
+            request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
+
+                request(app).post('/api/images').set("Cookie", resp.header['set-cookie'])
+                    .set('content-type', 'multipart/form-data')
+                    .field('description', 'Images uploaded through testing')
+                    .attach('image', fs.readFileSync(`${__dirname}/images/im1.png`), 'im1.png')
+                    .attach('image', fs.readFileSync(`${__dirname}/images/im2.png`), 'im2.png')
+                    .then((res) => {
+                        imageName = res.body.response[0].name
+                        // console.log(res.body)
+                        //     expect(res.body[0]).to.have.property('url')
+                        //     expect(res.body[0]).to.have.property('cloudinary_key')
+                        //     expect(res.body[0]).to.have.property('path')
+                        expect(res.statusCode).to.equal(201)
+                        expect(res.text).to.include('Successfully uploaded')
+                        done()
+                    }).catch((err) => done(err))
+            });
+        });
+        it("User should NOT be able to upload files that are not Images", function (done) {
+            this.timeout(20000)
+            request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
+
+                request(app).post('/api/images').set("Cookie", resp.header['set-cookie'])
+                    .set('content-type', 'multipart/form-data')
+                    .field('description', 'Images uploaded through testing')
+                    .attach('image', fs.readFileSync(`${__dirname}/images/vid1.mp4`), 'vid1.mp4')
+                    .attach('image', fs.readFileSync(`${__dirname}/images/vid2.mp4`), 'vid2.mp4')
+                    .then((res) => {
+                        expect(res.status).to.equal(400)
+                        expect(res.text).to.include("File type not supported! Must be an image type")
+                        done()
+                    }).catch((err) => done(err))
+            });
+        });
+
+        it("User should NOT be able to upload single or multiple images without been authenticated", function (done) {
+            // request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
+
+            request(app).post('/api/images')
+                .set('content-type', 'multipart/form-data')
+                .field('description', 'Images uploaded through testing')
+                .attach('image', fs.readFileSync(`${__dirname}/images/im1.png`), 'im1.png')
+                .attach('image', fs.readFileSync(`${__dirname}/images/im2.png`), 'im2.png')
+                .then((res) => {
+                    expect(res.statusCode).to.equal(401)
+                    expect(res.text).to.equal('Not authenticated')
+
+                    done()
+                }).catch((err) => done(err))
+            // });
+        });
+
+    });
+    describe("GET, Get Images", function () {
 
         it("User should be able to fetch images", function (done) {
             request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
 
                 request(app).get('/api/images').set("Cookie", resp.header['set-cookie'])
                     .then((res) => {
-                        expect(res.body).to.have.property('url')
-                        expect(res.body).to.have.property('cloudinary_key')
-                        expect(res.body).to.have.property('path')
+
+                        //expect(res.body).to.be.an('array')
+                        expect(res.body[0].url).to.be.an('string')
+                        expect(res.body[0]).to.have.property('url')
+                        expect(res.body[0]).to.have.property('cloudinary_key')
+                        expect(res.body[0]).to.have.property('path')
                         expect(res.statusCode).to.equal(200)
 
                         done()
                     }).catch((err) => done(err))
             })
-            it("User should not be able to fetch images without been authenticated", function (done) {
+            it("User should not be able to fetch images without authorization", function (done) {
 
                 request(app).get('/api/images')
                     .then((res) => {
-
                         expect(res.body.text).to.include('Not authenticated')
 
                         expect(res.statusCode).to.equal(401)
@@ -133,33 +192,28 @@ describe("User routes", function () {
         })
 
 
-    })
-
-
+    });
 
     describe("GET, Get Image by name", function () {
-        const imageName = 'ryntvntrydhpamwkunvh'
         it("User should be able to get image by name", function (done) {
             request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
 
                 request(app).get("/api/images/" + imageName).set("Cookie", resp.header['set-cookie'])
                     .then((res) => {
-                        expect(res.body).to.have.property('url')
-                        expect(res.body).to.have.property('cloudinary_key')
-                        expect(res.body).to.have.property('path')
+                        expect(res.body[0]).to.have.property('url')
+                        expect(res.body[0]).to.have.property('cloudinary_key')
+                        expect(res.body[0]).to.have.property('path')
                         expect(res.statusCode).to.equal(200)
 
                         done()
                     }).catch((err) => done(err))
             });
         });
-        it("User should NOT be able to get image by name without passing the right name", function (done) {
+        it("User should NOT be able to get image without passing the right name", function (done) {
             request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
                 const name = 'ryntvntrydhpamwkun' // wrong name
                 request(app).get("/api/images/" + name).set("Cookie", resp.header['set-cookie'])
                     .then((res) => {
-                        // expect(res.body[0]).to.have.property('url')
-                        // expect(res.body[0]).to.have.property('cloudinary_key')
                         expect(res.text).to.include('not found')
                         expect(res.statusCode).to.equal(404)
 
@@ -167,7 +221,7 @@ describe("User routes", function () {
                     }).catch((err) => done(err))
             });
         });
-        it("User should NOT be able to get images by name without been authenticated", function (done) {
+        it("User should NOT be able to get image without authorization", function (done) {
 
             request(app).get("/api/images/" + imageName)
                 .then((res) => {
@@ -182,51 +236,49 @@ describe("User routes", function () {
 
 
 
-    })
+    });
 
-
-    describe("POST, User upload Images", function () {
-        it("User should be able to upload single or multiple images", function (done) {
+    describe("DELETE, Delete Image by name", function () {
+        this.timeout(20000)
+        it("User should be able to delete image by name", function (done) {
             request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
 
-                request(app).post('/api/images').set("Cookie", resp.header['set-cookie'])
-                    .set('content-type', 'multipart/form-data')
-                    .field('description', 'Images uploaded through testing')
-                    .attach('image', fs.readFileSync(`${__dirname}/images/im1.png`), 'im1.png')
-                    .attach('image', fs.readFileSync(`${__dirname}/images/im2.png`), 'im2.png')
+                request(app).delete("/api/images/" + imageName).set("Cookie", resp.header['set-cookie'])
                     .then((res) => {
-
-                        //     expect(res.body[0]).to.have.property('url')
-                        //     expect(res.body[0]).to.have.property('cloudinary_key')
-                        //     expect(res.body[0]).to.have.property('path')
-                        expect(res.statusCode).to.equal(201)
-                        expect(res.text).to.equal('created')
+                        expect(res.text).to.include('Successfully deleted image')
+                        expect(res.statusCode).to.equal(200)
+                        done()
+                    }).catch((err) => done(err))
+            });
+        });
+        it("User should NOT be able to delete image  without passing the right name", function (done) {
+            request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
+                const name = 'ryntvntrydhpamwkun' // wrong name
+                request(app).get("/api/images/" + name).set("Cookie", resp.header['set-cookie'])
+                    .then((res) => {
+                        expect(res.text).to.include('not found')
+                        expect(res.statusCode).to.equal(404)
 
                         done()
                     }).catch((err) => done(err))
             });
         });
-        it("User should not be able to upload single or multiple images without been authenticated", function (done) {
-            // request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
+        it("User should NOT be able to delete image without authorization", function (done) {
 
-            request(app).post('/api/images')
-                .set('content-type', 'multipart/form-data')
-                .field('description', 'Images uploaded through testing')
-                .attach('image', fs.readFileSync(`${__dirname}/images/im1.png`), 'im1.png')
-                .attach('image', fs.readFileSync(`${__dirname}/images/im2.png`), 'im2.png')
+            request(app).get("/api/images/" + imageName)
                 .then((res) => {
-                    //     expect(res.body[0]).to.have.property('url')
-                    //     expect(res.body[0]).to.have.property('cloudinary_key')
-                    //     expect(res.body[0]).to.have.property('path')
+
+                    expect(res.text).to.include('Not authenticated')
+
                     expect(res.statusCode).to.equal(401)
-                    expect(res.text).to.equal('Not authenticated')
 
                     done()
                 }).catch((err) => done(err))
-            // });
-        });
+        })
 
-    })
+
+
+    });
 
     /** test cases for Images end */
 
@@ -235,6 +287,7 @@ describe("User routes", function () {
 
     describe("POST, User upload Videos", function () {
         it("User should be able to upload single or multiple videos", function (done) {
+            this.timeout(20000)
             request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
 
                 request(app).post('/api/videos').set("Cookie", resp.header['set-cookie'])
@@ -243,20 +296,31 @@ describe("User routes", function () {
                     .attach('video', fs.readFileSync(`${__dirname}/images/vid1.mp4`), 'vid1.mp4')
                     .attach('video', fs.readFileSync(`${__dirname}/images/vid2.mp4`), 'vid2.mp4')
                     .then((res) => {
-
-                        //     expect(res.body[0]).to.have.property('url')
-                        //     expect(res.body[0]).to.have.property('cloudinary_key')
-                        //     expect(res.body[0]).to.have.property('path')
-                        expect(res.statusCode).to.equal(201)
-                        expect(res.text).to.equal('created')
-
+                        //console.log(res.body.response[0])
+                        videoName = res.body.response[0].name;
+                        expect(res.status).to.equal(201)
+                        expect(res.text).to.include('Successfully uploaded')
                         done()
                     }).catch((err) => done(err))
             });
         });
-        it("User should NOT be able to upload single or multiple videos without been authenticated", function (done) {
-            // request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
+        it("User should NOT be able to upload files that are not videos", function (done) {
+            //this.timeout(20000)
+            request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
 
+                request(app).post('/api/videos').set("Cookie", resp.header['set-cookie'])
+                    .set('content-type', 'multipart/form-data')
+                    .field('description', 'Videos uploaded through testing')
+                    .attach('video', fs.readFileSync(`${__dirname}/images/im1.png`), 'im1.png')
+                    .attach('video', fs.readFileSync(`${__dirname}/images/im2.png`), 'im2.png')
+                    .then((res) => {
+                        expect(res.status).to.equal(400)
+                        expect(res.text).to.include("File type not supported! Must be a video type")
+                        done()
+                    }).catch((err) => done(err))
+            });
+        });
+        it("User should NOT be able to upload single or multiple videos without  authorization", function (done) {
             request(app).post('/api/videos')
                 .set('content-type', 'multipart/form-data')
                 .field('description', 'Video uploaded through testing')
@@ -268,10 +332,11 @@ describe("User routes", function () {
 
                     done()
                 }).catch((err) => done(err))
-            // });
+            
         });
 
     })
+
     describe("GET, Get Uploaded Videos", function () {
 
         it("User should be able to fetch videos", function (done) {
@@ -279,9 +344,9 @@ describe("User routes", function () {
 
                 request(app).get('/api/videos').set("Cookie", resp.header['set-cookie'])
                     .then((res) => {
-                        expect(res.body).to.have.property('url')
-                        expect(res.body).to.have.property('cloudinary_key')
-                        expect(res.body).to.have.property('path')
+                        expect(res.body[0]).to.have.property('url')
+                        expect(res.body[0]).to.have.property('cloudinary_key')
+                        expect(res.body[0]).to.have.property('path')
                         expect(res.statusCode).to.equal(200)
 
                         done()
@@ -292,7 +357,6 @@ describe("User routes", function () {
 
             request(app).get('/api/videos')
                 .then((res) => {
-
                     expect(res.text).to.include('Not authenticated')
 
                     expect(res.statusCode).to.equal(401)
@@ -304,15 +368,17 @@ describe("User routes", function () {
     })
 
     describe("GET, Get video by name", function () {
-        const videoName = 'ryntvntrydhpamwkunvh'
+        //const videoName = 'yppotweln8mqevf5fy33'
+
         it("User should be able to get video by name", function (done) {
             request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
 
                 request(app).get('/api/videos/' + videoName).set("Cookie", resp.header['set-cookie'])
                     .then((res) => {
-                        expect(res.body).to.have.property('url')
-                        expect(res.body).to.have.property('cloudinary_key')
-                        expect(res.body).to.have.property('path')
+                        console.log("wonder", videoName)
+                        expect(res.body[0]).to.have.property('url')
+                        expect(res.body[0]).to.have.property('cloudinary_key')
+                        expect(res.body[0]).to.have.property('path')
                         expect(res.statusCode).to.equal(200)
 
                         done()
@@ -348,6 +414,47 @@ describe("User routes", function () {
         })
     })
 
+    describe("DELETE, Delete Video by name", function () {
+        this.timeout(20000)
+        it("User should be able to delete video by name", function (done) {
+            request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
+
+                request(app).delete("/api/videos/" + videoName).set("Cookie", resp.header['set-cookie'])
+                    .then((res) => {
+                        expect(res.text).to.include('Successfully deleted video')
+                        expect(res.statusCode).to.equal(200)
+                        done()
+                    }).catch((err) => done(err))
+            });
+        });
+        it("User should NOT be able to delete video without passing the right name", function (done) {
+            request(app).post('/api/login').send({ email: 'alalnga@gmail.com', password: 'alalinga' }).then((resp) => {
+                const name = 'ryntvntrydhpamwkun' // wrong name
+                request(app).get("/api/videos/" + name).set("Cookie", resp.header['set-cookie'])
+                    .then((res) => {
+                        expect(res.text).to.include('not found')
+                        expect(res.statusCode).to.equal(404)
+
+                        done()
+                    }).catch((err) => done(err))
+            });
+        });
+        it("User should NOT be able to delete video without authorization", function (done) {
+
+            request(app).get("/api/images/" + videoName)
+                .then((res) => {
+
+                    expect(res.text).to.include('Not authenticated')
+
+                    expect(res.statusCode).to.equal(401)
+
+                    done()
+                }).catch((err) => done(err))
+        })
+
+
+
+    });
     /** test cases for videos end*/
 
 })
