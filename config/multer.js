e5787eb5ed6ea39605137c7ea4862af76d954baa
Alalinga <alalinga.mubarik@amalitech.org>
2022-03-25 03:34:55 +0000

    modified code

--- config/multer.js
+++ config/multer.js
@@ -2,19 +2,33 @@ const multer = require('multer')
 const path = require('path')
 const imageExt = ['.png', '.jpeg', '.jpg']
 const videoExt = ['.mp4', '.mkv', '.ts', '.wmv', '.avi', '.mov']
-upload = multer({
+uploadVideo = multer({
     storage: multer.diskStorage({}),
     fileFilter: (req, file, cb) => {
         // get file extention
         let filext = path.extname(file.originalname);
         //  checking if file extention exist in the array of extentions declared above
-        if (videoExt.includes(filext.toLocaleLowerCase()) || imageExt.includes(filext.toLocaleLowerCase())) {
+        if (videoExt.includes(filext.toLocaleLowerCase())) {
             return cb(null, true)
         }
         else {
-            return cb(new Error("File type not supported"), false)
+            return cb(new Error("File type not supported! Must be a video type"), false)
+        }
+    }
+});
+uploadImage = multer({
+    storage: multer.diskStorage({}),
+    fileFilter: (req, file, cb) => {
+        // get file extention
+        let filext = path.extname(file.originalname);
+        //  checking if file extention exist in the array of extentions declared above
+        if (imageExt.includes(filext.toLocaleLowerCase())) {
+            return cb(null, true)
+        }
+        else {
+            return cb(new Error("File type not supported! Must be an image type"), false)
         }
     }
 });
 
-module.exports = upload
+module.exports = {uploadImage,uploadVideo}
